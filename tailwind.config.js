85bc4144f297bea556678dc5cd3a2024067387b7
DLoake <repinfaust@gmail.com>
2025-09-18 17:02:35 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx,mdx}",
+    "./public/**/*.html"
+  ],
   theme: {
     extend: {
       colors: {
