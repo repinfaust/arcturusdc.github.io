69dd41bf3050fc276734907c93b82cb6cee81345
DLoake <repinfaust@gmail.com>
2025-09-23 16:52:31 +0100

    Update page.js

--- src/app/apps/page.js
+++ src/app/apps/page.js
@@ -1,5 +1,3 @@
-"use client";
-
 import Image from "next/image";
 import apps from "@/data/apps.json";
 import AppsClient from "./AppsClient";
@@ -16,6 +14,7 @@ export default function AppsPage() {
           alt=""
           fill
           priority
+          sizes="100vw"
           className="object-cover"
         />
         <div className="relative mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 py-24">
